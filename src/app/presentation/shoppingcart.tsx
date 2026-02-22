661e0b7dc02385bf8ded2b61e00b7326cc561e9e
emefaAngela <gelamengor@gmail.com>
2026-02-22 21:36:42 +0000

    added cart component

--- src/app/presentation/shoppingcart.tsx
+++ src/app/presentation/shoppingcart.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import supabase from "../../../utils/supabase";
-import BillingInfoCard from "../../components/billinginfocard";
+import Cart from "./shared/cart";
 import { useNavigate } from "react-router-dom";
 
 type Product = {
@@ -69,33 +69,7 @@ export default function ShoppingCart() {
           </table>
         </div>
 
-        <div className="flex flex-col w-72 h-96 space-y-4 border border-1 border-gray-200 rounded-lg items-center justify-center py-4">
-          <div className="text-2xl font-semibold">Order Summary</div>
-          <div className="grid grid-cols-2 px-10 gap-32">
-            <div className="flex flex-col space-y-2">
-              <div>Items</div>
-              <div>Sub Total</div>
-              <div>Shipping</div>
-              <div>Coupon discount</div>
-              <div className="pt-4">Total</div>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <div>3</div>
-              <div>$100</div>
-              <div>$10</div>
-              <div></div>
-              <div>$200</div>
-            </div>
-          </div>
-          <div
-            onClick={() => {
-              navigate("/billing-info");
-            }}
-            className="bg-green-600 text-white text-center rounded-4xl px-6 py-2"
-          >
-            Proceed to Checkout
-          </div>
-        </div>
+        <Cart/>
       </div>
     </div>
   );
--- src/components/billinginfocard.tsx
+++ src/components/billinginfocard.tsx
@@ -1,3 +1,5 @@
+import Cart from "../app/presentation/shared/cart";
+
 export default function BillingInfoCard() {
   return (
     <div className="w-full h-full m-0">
@@ -57,28 +59,7 @@ export default function BillingInfoCard() {
             </form>
           </div>
         </div>
-        <div className="flex flex-col w-72 h-96 space-y-4 border-1 border-gray-200 rounded-lg items-center justify-center py-4">
-          <div className="text-2xl font-semibold">Order Summary</div>
-          <div className="grid grid-cols-2 px-10 gap-32">
-            <div className="flex flex-col space-y-2">
-              <div>Items</div>
-              <div>Sub Total</div>
-              <div>Shipping</div>
-              <div>Coupon discount</div>
-              <div className="pt-4">Total</div>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <div>3</div>
-              <div>$100</div>
-              <div>$10</div>
-              <div></div>
-              <div>$200</div>
-            </div>
-          </div>
-          <div className="bg-green-600 text-white text-center rounded-4xl px-6 py-2">
-            Proceed to Checkout
-          </div>
-        </div>
+        <Cart />
       </div>
     </div>
   );
