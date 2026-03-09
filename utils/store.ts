65547d5a71daa24d9417a436655cdeb713cbab50
emefaAngela <gelamengor@gmail.com>
2026-03-09 05:25:14 +0000

    fixed bug with persisting state data in local storage

--- utils/store.ts
+++ utils/store.ts
@@ -5,13 +5,14 @@ import { persistReducer, persistStore } from "redux-persist";
 
 const persistConfig = {
   key: "root", // the key for the persist state in storage
-  storage, // the storage engine to use (localStorage in this case)
+  storage,
+  
 };
 
 const persistedReducer = persistReducer(persistConfig, checkoutSlice.reducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: { checkout: persistedReducer },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
