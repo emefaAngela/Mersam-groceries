6caec1d37427449110da2dccbb3f73a95f60a68c
emefaAngela <gelamengor@gmail.com>
2026-03-11 22:54:15 +0000

    cart ui fix

--- src/app/presentation/shoppingcart.tsx
+++ src/app/presentation/shoppingcart.tsx
@@ -12,7 +12,6 @@ export default function ShoppingCart() {
   );
   console.log(cartItems);
   const dispatch = useDispatch();
-  
 
   return (
     <div className="w-full h-full m-0">
@@ -20,7 +19,7 @@ export default function ShoppingCart() {
         <h1 className="text-3xl">Shop</h1>
         <span>Home / Shopping Cart</span>
       </div>
-      <div className="flex sm:flex-row flex-col space-y-8  sm:justify-between sm:mx-24 mx-auto items-center justify-center mb-16">
+      <div className="flex sm:flex-row w-full  text-xs sm:text-sm flex-col space-y-8  sm:justify-between sm:mx-24  items-center justify-center mb-16">
         <div className="flex">
           <table>
             <thead className="bg-yellow-300 h-12 rounded-lg flex flex-row px-4 py-1 space-x-8  sm:space-x-42 justify-center items-center font-semibold">
@@ -34,9 +33,9 @@ export default function ShoppingCart() {
               {cartItems.map((item: CartItem) => (
                 <tr
                   key={item.product?.id}
-                  className="flex flex-row justify-between sm:px-4 py-2 border-b"
+                  className="flex flex-row justify-between   sm:px-4 py-2 border-b"
                 >
-                  <td className="flex sm:flex-row flex-col space-x-1">
+                  <td className="flex ml-2 sm:flex-row text-center flex-col space-x-1">
                     <div className="sm:w-16 sm:h-16 h-12 w-12 p-2 border border-0.5 border-gray-200 rounded-md flex justify-center items-center">
                       <img
                         src={item.product?.image}
@@ -50,12 +49,12 @@ export default function ShoppingCart() {
                       {/* <div>{product.price}</div> */}
                     </div>
                   </td>
-                  <td className="-ml-12 sm:-ml-0"> {item?.product?.price}</td>
-                  <td className="text-center">{item?.quantity}</td>
-                  <td className="  text-left">
+                  <td className="-ml-8 sm:-ml-16 flex "> {item?.product?.price}</td>
+                  <td className="-ml-8 sm:-ml-8 text-center">{item?.quantity}</td>
+                  <td className="-ml-6 sm:-ml-4 text-left">
                     ${item?.product?.price * item?.quantity}
                   </td>
-                  <td className="text-center flex">
+                  <td className="text-center mr-4 flex">
                     <TrashIcon
                       className="w-4 h-4"
                       onClick={() => {
