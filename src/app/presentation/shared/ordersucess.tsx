5e5144d5b059a5a85ed0cdca43da19170189bf9a
emefaAngela <gelamengor@gmail.com>
2026-03-14 17:47:48 +0000

    added function to verify payment

--- src/app/presentation/shared/ordersucess.tsx
+++ src/app/presentation/shared/ordersucess.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { verifyPayment } from "../../../hooks/usePayment";
+
 export default function OrderSuccess() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const reference = params.get("reference");
+
+    verifyPayment(reference ?? "");
+  }, []);
   return (
     <div className="w-full h-full flex justify-center items-center  bg-black opacity-50 fixed  z-40">
       <div className=" flex flex-col space-y-4 items-center z-50 bg-gray-300 p-6  justify-center rounded-lg">
-        <div className="text-2xl font-semibold text-green-600">Order Successful!</div>
-        <div className="text-lg text-gray-700">Thank you for your purchase. Your order has been placed successfully.</div>
+        <div className="text-2xl font-semibold text-green-600">
+          Order Successful!
+        </div>
+        <div className="text-lg text-gray-700">
+          Thank you for your purchase. Your order has been placed successfully.
+        </div>
       </div>
     </div>
   );
--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -1,16 +1,36 @@
-export async function handleCheckout  (email: string, amount: number) {
- const response = await fetch("https://myyzytqvzcmfopbgqvfn.supabase.co/functions/v1/payment-api", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    email: "gelamengor@gmail.com",
-    amount: 1,
-  }),
-});
+export async function handleCheckout(email: string, amount: number) {
+  const response = await fetch(
+    "https://myyzytqvzcmfopbgqvfn.supabase.co/functions/v1/payment-api",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "gelamengor@gmail.com",
+        amount: 1,
+      }),
+    },
+  );
 
-const data = await response.json();
-console.log(data);
-window.location.href = data.data.authorization_url;
+  const data = await response.json();
+  console.log(data);
+  if (data?.data?.authorization_url) {
+    window.location.href = data.data.authorization_url;
+  } else {
+    console.error("Payment initialization failed:", data);
+  }
+}
+
+export async function verifyPayment(reference: string) {
+  const res = await fetch(
+    "https://myyzytqvzcmfopbgqvfn.supabase.co/functions/v1/verify-paystack-payment",
+    {
+      method: "POST",
+      body: JSON.stringify({ reference }),
+    },
+  );
+
+  const data = await res.json();
+  console.log(data);
 }
